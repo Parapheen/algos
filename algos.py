4089f296c92a4ebaa3df4b8c75f7cfcbecd0326d
Evgenii Petrovich <evpetrovich@yandex-team.ru>
2020-03-05 00:26:18 +0300

    сортировка выбором

--- algos.py
+++ algos.py
@@ -51,8 +51,26 @@ def binary_search(l, item):
             low = mid + 1
     return None
 
+# 5. Сортировка выбором по возрастанию
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def sorting(arr):
+    new_array = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_array.append(arr.pop(smallest))
+    return new_array
+
 
 if __name__ == '__main__':
     # print(is_correct('foo(bar);'))
     # print(tickets([25, 25, 25, 25, 50, 100, 50]))
-    print(binary_search([1, 3, 5, 7, 9], 3))
+    # print(binary_search([1, 3, 5, 7, 9], 3))
+    print(sorting([5, 3, 2, 1, 9, 4]))
